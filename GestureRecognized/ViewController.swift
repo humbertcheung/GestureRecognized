75c67f293c908e9d5c43e8a650926ba8f2efd000
Humbert Cheung <846379064@qq.com>
2018-09-21 09:39:12 +0800

    Initial Commit

---
+++ GestureRecognized/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GestureRecognized
+//
+//  Created by HumbertCheung on 2018/9/21.
+//  Copyright © 2018年 Humbert Cheung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
